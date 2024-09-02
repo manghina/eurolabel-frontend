15ffedc0e7a017d6c299595009f4bbd31b3c853a
Dario <webclosureinfo@gmail.com>
2024-09-02 15:56:38 +0200

    login url

--- src/app/demo/service/auth.service.ts
+++ src/app/demo/service/auth.service.ts
@@ -6,7 +6,7 @@ import { baseUrl } from "./constant";
 // Auth service
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    private _loginUrl = baseUrl + "login";
+    private _loginUrl = "https://app.eulabel.it/backend/" + "login";
     private userDetails = null;
     private refreshInterval = null;
     constructor(private http: HttpClient) { }
