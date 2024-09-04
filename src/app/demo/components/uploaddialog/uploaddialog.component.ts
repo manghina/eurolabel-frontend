39ad051490fa26ea30958a51dbcab44a44230338
Joinweb1977 <info@giacomofranci.it>
2024-09-04 12:43:47 +0200

    zoom cropped image

--- src/app/demo/components/uploaddialog/uploaddialog.component.ts
+++ src/app/demo/components/uploaddialog/uploaddialog.component.ts
@@ -12,6 +12,7 @@ interface ImageTransform {
   translateH?: number;
   translateV?: number;
   translateUnit?: '%' | 'px';
+  zoom?: number;
 }
 
 @Component({
@@ -21,7 +22,7 @@ interface ImageTransform {
 })
 export class UploadDialogComponent {
 
-  constructor(private sanitizer: DomSanitizer, private fb: FormBuilder) {}
+  constructor(private sanitizer: DomSanitizer, private fb: FormBuilder) { }
 
   @Output() upload = new EventEmitter<any>();
 
@@ -36,56 +37,56 @@ export class UploadDialogComponent {
     color: null,
     image: null,
 
-})
+  })
   public imageFile: any;
-
+  scale = 1;
   loading: boolean = false;
   canvasRotation = 0;
   translateH = 0;
   translateV = 0;
   transform: ImageTransform = {
-      scale: 1,
-      rotate: 0,
-      flipH: false,
-      flipV: false,
-      translateUnit: 'px'
-    };
+    scale: 1,
+    rotate: 0,
+    flipH: false,
+    flipV: false,
+    translateUnit: 'px'
+  };
 
 
   fileChangeEvent(event: any): void {
-      this.imageChangedEvent = event;
+    this.imageChangedEvent = event;
   }
 
-  imageLoaded(event:any) {
+  imageLoaded(event: any) {
 
   }
-  loadImageFailed () {
+  loadImageFailed() {
     console.log('Load failed');
   }
   cropperReady() {
     debugger
   }
   onUpload(event) {
-      for(let file of event.files) {
-          this.uploadedFiles.push(file);
-      }
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
     }
+  }
 
   onSelect(e: FileSelectEvent) {
-    this.imageFile =e.files[0];
+    this.imageFile = e.files[0];
   }
 
   imageCropped(event: any) {
-      const imageUrl = event.objectUrl || event.base64 || '';
-      this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(imageUrl);
-      if (imageUrl.startsWith('data:')) {
-        const base64Data = imageUrl.split(',')[1];
-        const mimeType = imageUrl.split(';')[0].split(':')[1];
+    const imageUrl = event.objectUrl || event.base64 || '';
+    this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+    if (imageUrl.startsWith('data:')) {
+      const base64Data = imageUrl.split(',')[1];
+      const mimeType = imageUrl.split(';')[0].split(':')[1];
       //   this.imageBase64 = base64Data;
       //   this.imageExtension = mimeType.split('/')[1];
-        this.applyTransform();
-      }
-      this.upload.emit(event)
+      this.applyTransform();
+    }
+    this.upload.emit(event)
   }
 
   rotateLeft() {
@@ -104,37 +105,52 @@ export class UploadDialogComponent {
     });
   }
 
-  private flipAfterRotate() {
-    const flippedH = this.transform.flipH;
-      const flippedV = this.transform.flipV;
+  zoom(event: any) {
+    this.loading = true;
+    setTimeout(() => {
       this.transform = {
         ...this.transform,
-        flipH: flippedV,
-        flipV: flippedH
+        scale: this.scale
       };
-      this.translateH = 0;
-      this.translateV = 0;
-    }
+    });
+
+  }
+
+
+
+  private flipAfterRotate() {
+    const flippedH = this.transform.flipH;
+    const flippedV = this.transform.flipV;
+    this.transform = {
+      ...this.transform,
+      flipH: flippedV,
+      flipV: flippedH
+    };
+    this.translateH = 0;
+    this.translateV = 0;
+  }
 
   private applyTransform() {
-      const cropperImageElement = document.querySelector('.cropper-wrapper img') as HTMLElement;
-      const croppedImageElement = document.querySelector('.popup-image') as HTMLElement;
-      const { scale, rotate, flipH, flipV } = this.transform;
-      let transformString = `scale(${scale || 1}) rotate(${rotate || 0}deg)`;
-      if (flipH) {
-        transformString += ' scaleX(-1)';
-      }
-      if (flipV) {
-        transformString += ' scaleY(-1)';
-      }
-      if (cropperImageElement) {
-        cropperImageElement.style.transform = transformString;
-      }
-      if (croppedImageElement) {
-        croppedImageElement.style.transform = transformString;
-      }
+    const cropperImageElement = document.querySelector('.cropper-wrapper img') as HTMLElement;
+    const croppedImageElement = document.querySelector('.popup-image') as HTMLElement;
+    const { scale, rotate, flipH, flipV } = this.transform;
+
+    let transformString = `scale(${scale || 1}) rotate(${rotate || 0}deg)`;
+
+    if (flipH) {
+      transformString += ' scaleX(-1)';
     }
+    if (flipV) {
+      transformString += ' scaleY(-1)';
+    }
+    if (cropperImageElement) {
+      cropperImageElement.style.transform = transformString;
+    }
+    if (croppedImageElement) {
+      croppedImageElement.style.transform = transformString;
+    }
+  }
 
 
-  
+
 }
--- src/app/demo/components/uploaddialog/uploaddialog.module.ts
+++ src/app/demo/components/uploaddialog/uploaddialog.module.ts
@@ -6,12 +6,14 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 import { TranslateModule } from '@ngx-translate/core';
 import { FileUploadModule } from 'primeng/fileupload';
 import { ImageCropperComponent  } from 'ngx-image-cropper';
+import {SliderModule} from 'primeng/slider';
+
 
 @NgModule({
   declarations: [
     UploadDialogComponent
   ],
-  imports: [CommonModule, FileUploadModule, FormsModule, ReactiveFormsModule, RadioButtonModule, TranslateModule, ImageCropperComponent],
+  imports: [CommonModule, FileUploadModule, FormsModule, ReactiveFormsModule, RadioButtonModule, TranslateModule, ImageCropperComponent,SliderModule],
   exports: [UploadDialogComponent]
 })
 export class UploadDialogModule { } 
